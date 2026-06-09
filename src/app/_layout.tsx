6f564546b2da12314b0ab3d0e4bdefd41f355015
ash-g773 <aishwaryageorge@ymail.com>
2026-06-09 16:23:53 +0100

    Add login page, global styling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  const router = useRouter();
+
+  let isAuth = false;
+
+  useEffect(() => {
+    if (!isAuth) {
+      router.replace("/(auth)/login");
+    } else {
+      router.replace("/(tabs)");
+    }
+  });
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(auth)" />
+    </Stack>
+  );
 }
