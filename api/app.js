aa149e4ac7f23ae91ad58d0b555663ba618042b9
Anthony-Omo <ajomotayo17@gmail.com>
2026-06-09 18:22:49 +0100

    null

--- api/app.js
+++ api/app.js
@@ -6,11 +6,4 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    title: "API",
-    description: "Lost pet recovery API",
-  });
-});
-
 module.exports = app;
