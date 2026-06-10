af5b97091292c261e08cffa77a84efc9576488fa
ella <ejgsimmons@outlook.com>
2026-06-10 12:07:45 +0100

    temporarily redirects straight to map page

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,19 @@
-import { Text, View } from "react-native";
-export default function TabsIndex() {
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
+
+export default function MapScreen() {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
