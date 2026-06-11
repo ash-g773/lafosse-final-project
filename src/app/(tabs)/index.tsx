5e7c76ddc6a6d9f4e290b30dadf75513f8bd6eb0
ella <ejgsimmons@outlook.com>
2026-06-11 10:40:31 +0100

    downgrade testing library for build

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
 import { StyleSheet, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 export default function MapScreen() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        provider={PROVIDER_DEFAULT}
+        style={styles.map}
+        initialRegion={{
+          latitude: 51.5074,
+          longitude: -0.1278,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      />
     </View>
   );
 }
