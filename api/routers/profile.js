3f392b357e2cca6b8218faf41f6c31bd2fe738cb
codemustafa <15mohamedm@gmail.com>
2026-06-11 15:18:58 +0100

    authentication added for profie route

--- api/routers/profile.js
+++ api/routers/profile.js
@@ -1,11 +1,12 @@
 const { Router } = require("express")
 const profileController = require("../controller/profile")
+const authenticator = require("../middleware/authenticator")
 
 const profileRouter = Router()
 
-profileRouter.get("/:users_id", profileController.getProfile)
-profileRouter.patch("/:users_id", profileController.updateProfile)
-profileRouter.get("/:users_id/pets", profileController.getUserPets)
-profileRouter.get("/:users_id/sightings", profileController.getUserSightings)
+profileRouter.get("/:users_id", authenticator, profileController.getProfile)
+profileRouter.patch("/:users_id", authenticator, profileController.updateProfile)
+profileRouter.get("/:users_id/pets", authenticator, profileController.getUserPets)
+profileRouter.get("/:users_id/sightings", authenticator, profileController.getUserSightings)
 
 module.exports = profileRouter
