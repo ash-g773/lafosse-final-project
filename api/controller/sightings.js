3896682db9074d6a5f382d3cf5cb246d40b82155
codemustafa <15mohamedm@gmail.com>
2026-06-11 14:40:35 +0100

    created functions for getting all sightings and by id

--- api/controller/sightings.js
+++ api/controller/sightings.js
@@ -1,13 +1,32 @@
 const Sighting = require("../model/Sighting");
 
-async function create(req, res) {
-   try {
-    const data = req.body;
-    const newSighting = await Sighting.create(data);
-    res.status(201).json(newSighting);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  } 
+async function index(req, res) {
+    try {
+        const sightings = await Sighting.getAll();
+        res.status(200).json(sightings);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 }
 
-module.exports = { create };
+async function show(req, res) {
+    try {
+        const id = parseInt(req.params.id);
+        const sighting = await Sighting.getOneById(id);
+        res.status(200).json(sighting);
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+}
+
+async function create(req, res) {
+    try {
+        const data = req.body;
+        const newSighting = await Sighting.create(data);
+        res.status(201).json(newSighting);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+module.exports = { index, show, create };
