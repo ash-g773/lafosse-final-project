9cdc20b5154adbf310f22b29ed6e79e8b3391727
ella <ejgsimmons@outlook.com>
2026-06-15 15:07:43 +0100

    login tests

--- __tests__/login-screen-test.tsx
+++ __tests__/login-screen-test.tsx
@@ -1,5 +1,23 @@
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 import LoginScreen from "../src/app/(auth)/login";
+
+jest.mock("expo-router", () => {
+  return {
+    router: {
+      push: jest.fn(),
+      replace: jest.fn(),
+    },
+    Stack: {
+      Screen: () => null,
+    },
+  };
+});
+
+const mockedRouter = require("expo-router").router;
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
 describe("login screen tests", () => {
   it("should have a logo", async () => {
@@ -23,5 +41,15 @@ describe("login screen tests", () => {
 
     expect(password).toBeTruthy();
     expect(password.type).toBe("TextInput");
+  });
+  it("redirects to register page when button is pressed", async () => {
+    const { getByTestId } = await render(<LoginScreen />);
+    fireEvent.press(getByTestId("register-btn"));
+    expect(mockedRouter.push).toHaveBeenCalledWith("/(auth)/register");
+  });
+  it("redirects to report sighting when button is pressed", async () => {
+    const { getByTestId } = await render(<LoginScreen />);
+    fireEvent.press(getByTestId("report-btn"));
+    expect(mockedRouter.replace).toHaveBeenCalledWith("/(auth)/reportSighting");
   });
 });
--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -109,20 +109,19 @@ export default function LoginScreen() {
         <View style={styles.register}>
           <Text style={styles.linkButtonText}>Don't have an account?</Text>
 
-          <TouchableOpacity style={styles.linkButton}>
-            <Text
-              style={styles.linkButtonTextBold}
-              onPress={() => router.push("/(auth)/register")}
-            >
-              {" "}
-              Register Here
-            </Text>
+          <TouchableOpacity
+            style={styles.linkButton}
+            testID="register-btn"
+            onPress={() => router.push("/(auth)/register")}
+          >
+            <Text style={styles.linkButtonTextBold}> Register Here</Text>
           </TouchableOpacity>
         </View>
 
         <View>
           <TouchableOpacity
             style={styles.toReport}
+            testID="report-btn"
             onPress={() => router.replace("/(auth)/reportSighting")}
           >
             <Text style={styles.buttonText}>Report a sighting</Text>
