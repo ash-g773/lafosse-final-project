55dbc66c7018e95e11beb22d795f8ce20d6d8352
ella <ejgsimmons@outlook.com>
2026-06-16 10:54:19 +0100

    downgrade gemini version

--- api/utils/gemini.utils.js
+++ api/utils/gemini.utils.js
@@ -50,7 +50,7 @@ async function getAiMatches(pet, sightings) {
   }, 15000);
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash:generateContent?key=${process.env.GEMINI_MATCHING_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_MATCHING_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
