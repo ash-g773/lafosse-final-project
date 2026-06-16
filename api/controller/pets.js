b407f09f3fd84a19484645304fe60120064f21f6
codemustafa <15mohamedm@gmail.com>
2026-06-16 16:46:03 +0100

    updated as 4 arguments now created instead of 3

--- api/controller/pets.js
+++ api/controller/pets.js
@@ -32,7 +32,7 @@ async function create(req, res) {
     const newPet = await Pet.create(data)
 
     await Alert.createForAllUsers(
-      newPet.pets_id,
+      newPet.pets_id,null,
       "lost",
       `Lost ${newPet.species}: ${newPet.name}${newPet.colour ? " · " + newPet.colour : ""} near ${newPet.last_seen_location}`
     )
